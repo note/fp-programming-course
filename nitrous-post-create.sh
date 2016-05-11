b485f4b281556948f4dc6f4dc2ae3d520bce631e
Michał Grygierzec <michal.grygierzec@gmail.com>
2016-05-11 20:52:05 +0200

    add installing GHC step in automatic nitrous vm setup

--- nitrous-post-create.sh
+++ nitrous-post-create.sh
@@ -5,3 +5,5 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689
 echo 'deb http://download.fpcomplete.com/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/fpco.list
 
 sudo apt-get update && sudo apt-get install stack -y
+
+stack setup
