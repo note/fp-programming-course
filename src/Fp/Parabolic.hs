0976cbed1a5e53dd0f37965192870d21cd78c38a
Michal Sitko <michal.sitko@sap.com>
2016-05-14 15:09:55 +0200

    dodane polecenia

--- src/Fp/Parabolic.hs
+++ src/Fp/Parabolic.hs
@@ -3,6 +3,9 @@ module Fp.Parabolic
     findRoots
 ) where
 
+-- Twoim zadaniem jest zaimplementowanie funkcji, ktora znajdzie miejsca zerowe zadanej funkcji kwadratowej
+-- Wspolczynniki a, b, c sa przekazywane jako parameter funkcji
+-- Odpowiednie wzory mozesz znalezc na http://www.matematykam.pl/miejsca_zerowe_funkcji_kwadratowej.html
 findRoots :: (Floating a, Ord a) => a -> a -> a -> [a]
 findRoots a b c =
     if (delta > 0) then
--- src/Fp/Various.hs
+++ src/Fp/Various.hs
@@ -3,6 +3,10 @@ module Fp.Various
     sum_below
 ) where
 
+-- liczby podzielne przez 3 lub 5 ponizej 10 to 3, 5, 6, 9.
+-- 3 + 5 + 6 + 9 = 23
+-- Napisz funkcje, ktora przyjmuje ograniczenie (w przykladzie bylo to 10) jako parametr i zwraca sume
+-- liczb podzielnych przez 3 lub 5 mniejszych niz to ograniczenie
 sum_below :: Integer -> Integer
 sum_below limit = sum below
     where
