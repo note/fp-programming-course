bcdc18a48ba941cde0f9d15d6fc1fd8616c4e16f
Michał Grygierzec <michal.grygierzec@gmail.com>
2016-05-14 15:22:56 +0200

    Clean 08_lambda

--- exercises/your_where.hs
+++ exercises/your_where.hs
@@ -2,15 +2,8 @@
 -- 2. let y = 10; x = 10 * 5 + y in x * 5
 -- 3. let x = 7; y = negate x; z = y * 10 in z / x + y
 
-ex1 = x * 3 + y
-    where x = 3
-          y = 1000
+ex1 = undefined
           
-ex2 = x * 5
-    where y = 10
-          x = 10 * 5 + y
+ex2 = undefined
      
-ex3 = z / x + y
-    where x = 7 
-          y = negate x
-          z = y * 10
+ex3 = undefined
